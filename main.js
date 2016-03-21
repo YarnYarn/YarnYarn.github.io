2fa64a763412b359c0f66c7d5854084af3b98675
YarnYarn <aethne.mitchell@ryerson.ca>
2016-03-20 22:38:03 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+function init(){
+	var paper = new Raphael('maindiv',800,600);
+	
+	var renderer = new Renderer(paper,800,600);
+	
+	renderer.addVector(new Vector3(50,0,0));
+	renderer.addVector(new Vector3(0,50,0));
+	renderer.addVector(new Vector3(0,0,50));
+	renderer.addVector(new Vector3(50,0,0));
+	renderer.addVector(new Vector3(0,0,0));
+	renderer.addVector(new Vector3(0,50,0));
+	renderer.addVector(new Vector3(0,0,0));
+	renderer.addVector(new Vector3(0,0,50));
+
+	var lastTime = new Date().getTime();
+
+
+	renderer.scale(4,4,4);
+	renderer.translate(0,100,0);
+
+
+	setInterval(function(){
+		renderer.drawLines();
+		var dt = new Date().getTime() - lastTime;
+		lastTime = new Date().getTime();
+		renderer.rotate(0.001*dt,-0.0005*dt,0.00025*dt);
+	},1/20);
+}
